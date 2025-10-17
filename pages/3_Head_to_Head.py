186c9eb577fc7e2524a72163067485d9a49662a1
ckelly668 <ckelly668@qub.ac.uk>
2025-10-17 16:30:12 +0100

    how to read table

--- pages/3_Head_to_Head.py
+++ pages/3_Head_to_Head.py
@@ -24,5 +24,15 @@ st.pyplot(fig_head_to_head_table, width='content')
 fig_league_table = create_league_table(st.session_state.League.build_league_table_from_matrix())
 # st.pyplot(fig_league_table, width='content')
 
+
+st.header("How to Read Table")
+st.markdown("""
+    - Each **row** represents the results of the team listed on the left against all other teams in the league.
+    - The columns represent the opposing teams.
+    - The scores are listed as **(Sets Won, Sets Lost)**, e.g., **2-1** means the team won 2 sets and lost 1 set against that opponent.
+    - Green cells indicate a **win** for the team listed in the row, while red cells indicate a **loss**.
+    - A dash ("-") indicates that the match has not yet been played.
+    """)
+
 # ================= Add Logo  ==========================
 st.sidebar.image('images/padel_logo_2.png', width=300)
